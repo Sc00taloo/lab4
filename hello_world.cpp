2ce7d1052ed5f6718d78a8118b7a3864b01435b3
Sc00taloo <v4nyapop0v12@gmail.com>
2023-10-06 23:59:15 +0300

    update again hello world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
   string name; \\name
   std::cin >> name; \\input name.
   std::cout << "Hello world from " << name << std::endl; \\Вывод hello world
